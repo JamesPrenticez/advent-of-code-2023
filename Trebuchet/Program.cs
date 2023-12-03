cde18a4468e22fa2d68f3771f326b8fce019f1e6
James Prentice <jamesprenticez@gmail.com>
2023-12-03 15:34:38 +1300

    Treuchet - Task Two

--- Trebuchet/Program.cs
+++ Trebuchet/Program.cs
@@ -8,6 +8,8 @@ class Program {
     Console.WriteLine($"Task One: {TaskOne(filePath)}");
     Console.WriteLine($"========================================");
     Console.WriteLine($"Task One Improved: {TaskOneImproved(data)}");
+    Console.WriteLine($"========================================");
+    Console.WriteLine($"Task Two: {TaskTwo(data)}");
   }
 
   static string TaskOne(string filePath){
@@ -71,7 +73,7 @@ class Program {
     return '\0'; // Return null character if no digit is found
   }
 
-  static string TaskOneImproved(string data){
+  static string TaskOneImproved(string data) {
     var lines = data.Split('\n');
 
     int totalNumber = 0;
@@ -90,6 +92,76 @@ class Program {
     return totalNumber.ToString();
   }
 
+  static string TaskTwo(string data) {
+    var lines = data.Split('\n');
+    int totalNumber = 0;
+    var numbersAsText = new[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+    var numbersList = new[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+    foreach (var line in lines){
+      var firstNumber = FindFirst(numbersAsText, numbersList, line);
+      var lastNumber = FindLast(numbersAsText, numbersList, line);
+      var stringNumber = $"{firstNumber}{lastNumber}";
+      totalNumber += int.Parse(stringNumber);
+    }
+
+    return totalNumber.ToString();
+  }
+
+  static int FindFirst(string[] numbersAsText, string[] numbersList, string line) {
+    int lowestIteration = line.Length;
+    string currentUsedValue = "";
+
+    foreach (var currentNumber in numbersAsText.Concat(numbersList)){
+      var firstIndex = line.IndexOf(currentNumber);
+      if (firstIndex != -1 && firstIndex <= lowestIteration){
+        lowestIteration = firstIndex;
+        currentUsedValue = currentNumber;
+      }
+    }
+
+    return ConvertToNumber(currentUsedValue);
+  }
+
+  static int FindLast(string[] numbersAsText, string[] numbersList, string line){
+    int highestIteration = 0;
+    string currentUsedValue = "";
+
+    foreach (var currentNumber in numbersAsText.Concat(numbersList)){
+      var lastIndex = line.LastIndexOf(currentNumber);
+      if (lastIndex != -1 && lastIndex >= highestIteration)
+      {
+        highestIteration = lastIndex;
+        currentUsedValue = currentNumber;
+      }
+    }
+
+    return ConvertToNumber(currentUsedValue);
+  }
+
+  static int ConvertToNumber(string number) {
+    return number switch {
+      "one" => 1,
+      "two" => 2,
+      "three" => 3,
+      "four" => 4,
+      "five" => 5,
+      "six" => 6,
+      "seven" => 7,
+      "eight" => 8,
+      "nine" => 9,
+      "1" => 1,
+      "2" => 2,
+      "3" => 3,
+      "4" => 4,
+      "5" => 5,
+      "6" => 6,
+      "7" => 7,
+      "8" => 8,
+      "9" => 9,
+      _ => throw new Exception($"ERROR {number}")
+    };
+  }
 
 }
 
